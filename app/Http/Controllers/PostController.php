94459327f7f757b407212ba797f9754095e1b545
shirakou <koudai126126@icloud.com>
2024-06-07 22:47:37 +0900

    変更をコミット

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class Postcontroller extends Controller
 {
     public function index() {
         $posts =DB::table('posts')->get();
         return view('posts.index',compact('posts'));
+    }
+
+    public function show($id) {
+        $post = Post::find($id);
+
+        return view('posts.show',compact('post'));
     }
 }
